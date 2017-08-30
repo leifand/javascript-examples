80e9a9c8f745f9f1194ca86fe6b1da6f55944381
Leif Anderson <leif.v.anderson@gmail.com>
2017-08-30 12:51:12 -0500

    mergesort

--- mergesort/merge_sort.js
+++ mergesort/merge_sort.js
@@ -1,17 +1,15 @@
 /* merge_sort.js */
 
-function merge (arr1, arr2) { // arr2 merged into arr1
-
+// arr1 is longer or equal in length to arr2
+// arr1 and arr2 are sorted
+//
+function merge (arr1, arr2) {
   var len1 = arr1.length;
   var len2 = arr2.length;
   var index1 = 0;
   var index2 = 0;
-
-  // while !(index1 > len1 || index2 > len2)
-  while (index1 < len1 && index2 < len2) { // fails on?
-
-    if (arr1[index1] < arr2[index2])
-      index1++;
+  while (index1 < len1 && index2 < len2) {
+    if (arr1[index1] < arr2[index2]) index1++;
     else { // grow arr1 and increment counters
       arr1.splice(index1, 0, arr2[index2]);
       index1++;
@@ -19,24 +17,73 @@ function merge (arr1, arr2) { // arr2 merged into arr1
       index2++;
     }
   }
-
-  // append any remaining elements from arr2
-  while (index2 < len2) {
+  while (index2 < len2) {  // append any remaining elements from arr2
     arr1.push(arr2[index2]);
     index2++;
   }
-
   return arr1;
 }
 
+// the classic! O(n log n)
+//
 function merge_sort(arr) {
-  var mid = arr.length / 2;
-  left = arr.slice(0, mid);
-  right = arr.slice(mid);
-  if (left.length == 1 && right.length == 1)
-    return merge(left, right);
-  else if (left.length == 1 && right.length !=1)
-    return merge(merge_sort(left), right);
-  else
-    return merge(merge_sort(left), merge_sort(right));
+  if (arr.length < 2) return arr;
+  else {
+    var split_point = arr.length/2;
+    var left = arr.slice(0, split_point);
+    var right = arr.slice(split_point, arr.length);
+    return merge(merge_sort(left),merge_sort(right));
+  }
+}
+
+function test_merge_sort(arr_size) {
+  test_array = [];
+  for (var i=0; i<arr_size; i++) test_array[i] = Math.round(1 + (Math.random() * (arr_size - 1)));
+  var start_time = Date.now()/1000;
+  var ret = merge_sort(test_array);
+  var end_time = Date.now()/1000;
+  console.log(end_time - start_time);
+  return ret;
+}
+
+//x = [1,17,-2,0,9,6,7,11,2000,-10, 17, 100000];
+//console.log(merge_sort(x));
+
+function test_merge1() { // big arrays !!!
+  var array1 = [];
+  var array2 = [];
+  var ret = [];
+  for (var i=0; i<10000000; i++) array1[i] = i;
+  for (var j=0; j<10000000; j++) array2[j] = j + 10000000;
+  var start_time = Date.now()/1000;
+  ret = merge(array1, array2);
+  var end_time = Date.now()/1000;
+  console.log(end_time - start_time);
+  return ret;
+}
+
+function test_merge2() { // out of order, worst possible case
+  var array1 = [];
+  var array2 = [];
+  var ret = [];
+  for (var i=0; i<200000; i++) array1[i] = i;
+  for (var j=0; j<200000; j++) array2[j] = j + 200000;
+  var start_time = Date.now()/1000;
+  ret = merge(array2, array1);
+  var end_time = Date.now()/1000;
+  console.log(end_time - start_time);
+  return ret;
+}
+
+function test_merge3() { // arrays share values
+  var array1 = [];
+  var array2 = [];
+  var ret = [];
+  for (var i=0; i<200000; i++) array1[i] = i;
+  for (var j=0; j<200000; j++) array2[j] = j + 1;
+  var start_time = Date.now()/1000;
+  ret = merge(array1, array2);
+  var end_time = Date.now()/1000;
+  console.log(end_time - start_time);
+  return ret;
 }
