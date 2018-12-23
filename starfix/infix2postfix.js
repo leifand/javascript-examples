a88ffcf61aeb8f78f667c0f0a7d441773762f334
Leif Anderson <leif.v.anderson@gmail.com>
2018-12-22 21:48:24 -0600

    parameterized precedence functions

--- starfix/infix2postfix.js
+++ starfix/infix2postfix.js
@@ -7,17 +7,17 @@ A basic (hardcoded) compiler.
 
 */
 
-const infix2postfix = {};
+const parser = {};
 
-infix2postfix.operands = ['+','-','*','/','(',')'];
+parser.operands = ['+','-','*','/','(',')'];
 
-infix2postfix.operators = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E'];
+parser.operators = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E'];
 
-infix2postfix.valid_operator = (token) => {return infix2postfix.operands.includes(token);}
+parser.valid_operator = (token) => {return parser.operands.includes(token);}
 
-infix2postfix.valid_operand = (token) => {return infix2postfix.operators.includes(token);}
+parser.valid_operand = (token) => {return parser.operators.includes(token);}
 
-infix2postfix.infix_priority = (token) => { 
+parser.infix_priority = (token) => { 
     let ret = -1;
     if (token == '*' || token == '/') ret = 2;
     else if (token == '+' || token == '-') ret = 1;
@@ -26,7 +26,7 @@ infix2postfix.infix_priority = (token) => {
     return ret;
 }
 
-infix2postfix.stack_priority = (token) => {
+parser.stack_priority = (token) => {
     let ret = -1;
     if (token == '*' || token == '/') ret = 2;
     else if (token == '+' || token == '-') ret = 1;
@@ -35,14 +35,14 @@ infix2postfix.stack_priority = (token) => {
     return ret;    
 }
 
-infix2postfix.convert = (expression) => {
+parser.convert = (expression, infix_prec=parser.infix_priority, postfix_prec=parser.stack_priority) => {
     let item, new_token = null;
     const op_stack = [];
     const queue = [];
     op_stack.push('#'); // terminal symbol
     for (let i=0; i < expression.length; i++) {
         new_token = expression[i];
-        if (infix2postfix.valid_operand(new_token)) {
+        if (parser.valid_operand(new_token)) {
             queue.push(new_token);
         } else if (new_token == ')') {
             item = op_stack.pop();
@@ -55,9 +55,9 @@ infix2postfix.convert = (expression) => {
                 item = op_stack.pop();
                 queue.push(item);
             } // end while
-        } else if (infix2postfix.valid_operator(new_token)) {
+        } else if (parser.valid_operator(new_token)) {
             item = op_stack.pop();
-            while (infix2postfix.stack_priority(item) >= infix2postfix.infix_priority(new_token)) {
+            while (postfix_prec(item) >= infix_prec(new_token)) {
                 queue.push(item);
                 item = op_stack.pop();
             } // end while
@@ -70,14 +70,14 @@ infix2postfix.convert = (expression) => {
     return queue; // *magic*
 }
 
-infix2postfix.valueOf = (token) => {
-    for (let i=0; i<infix2postfix.operators.length; i++) {
-        if (token == infix2postfix.operators[i]) return i;
+parser.valueOf = (token) => {
+    for (let i=0; i<parser.operators.length; i++) {
+        if (token == parser.operators[i]) return i;
     }
     return -1;
 }
 
-infix2postfix.eval = (a, b, oper) => {
+parser.eval = (a, b, oper) => {
     if (oper == '+') return (a+b);
     else if (oper == '-') return (a-b);
     else if (oper == '*') return (a*b);
@@ -85,20 +85,20 @@ infix2postfix.eval = (a, b, oper) => {
     else return -1;
 }
 
-infix2postfix.evaluate = (postfix_expression) => {
+parser.evaluate = (postfix_expression) => {
     let token = '';
     const stack = [];
-    let v1, v2, top = 0.0;
+    let v1, v2 = 0.0;
     for (let i=0; i<postfix_expression.length; i++) {
         token = postfix_expression[i];
-        if (infix2postfix.valid_operand(token)) {
-            stack.push(infix2postfix.valueOf(token));
+        if (parser.valid_operand(token)) {
+            stack.push(parser.valueOf(token));
         } else if (token == '#') {
-            return stack.pop();
+            return stack.pop(); // solved via ugly logic
         } else {
             v2 = stack.pop();
             v1 = stack.pop();
-            stack.push(infix2postfix.eval(v1, v2, token));
+            stack.push(parser.eval(v1, v2, token));
         }
     }
     return -1;
@@ -106,15 +106,15 @@ infix2postfix.evaluate = (postfix_expression) => {
 
 x = 'A*B+(C-D/E)#';
 y = '(1+2)/3+9*(1/9)#';
-z = infix2postfix.convert(x); // #iheartjs
+z = parser.convert(x); // #iheartjs
 console.log(z);
 
-test = infix2postfix.convert(y);
+test = parser.convert(y);
 console.log(test);
-console.log(infix2postfix.evaluate(test));
+console.log(parser.evaluate(test));
 
-test2 = infix2postfix.convert('(7*5)-(6/7)*(3+(6/7)-(2/3))#');
+test2 = parser.convert('(7*5)-(6/7)*(3+(6/7)-(2/3))#');
 console.log(test2);
-console.log(infix2postfix.evaluate(test2));
+console.log(parser.evaluate(test2));
 
-module.exports = infix2postfix;
+module.exports = parser;
