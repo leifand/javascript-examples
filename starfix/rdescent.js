754f9b669da44225a89d4bdf630cb600be68dd40
Leif Anderson <leif.v.anderson@gmail.com>
2018-12-23 22:57:19 -0600

    working ha ha

--- starfix/rdescent.js
+++ starfix/rdescent.js
@@ -12,9 +12,9 @@ rparser.operators = ['+','-','*','/','(',')'];
 
 rparser.operands = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E'];
 
-rparser.valid_operand = (token) => {return parser.operands.includes(token);}
+rparser.valid_operand = (token) => {return rparser.operands.includes(token);}
 
-rparser.valid_operator = (token) => {return parser.operators.includes(token);}
+rparser.valid_operator = (token) => {return rparser.operators.includes(token);}
 
 rparser.top_level_expression = (expression) => {
     let syntax_error = false;
@@ -47,30 +47,58 @@ rparser.verify_add_factor = (expression, token, syntax_error) => {
 
 rparser.verify_expression = (expression, token, syntax_error) => {
     rparser.verify_add_factor(expression, token, syntax_error);
-    if (!syntax_error) verify_add_factor(expression, token, syntax_error);
+    if (!syntax_error) rparser.verify_add_factor(expression, token, syntax_error);
 }
 
 rparser.verify_primary = (expression, token, syntax_error) => {
     if (rparser.valid_operand(token)) {
-        
-
-
-
-
-
-        if (expression) {
-            token = expression.shift();
-            rparser.verify_expression(expression, token, syntax_error);
-        }
-        else {
-            syntax_error = true;
-        }
-        if (!syntax_error) {
-            if (token != ')') {
+        if (expression) token = expression.shift();
+        else { // not a valid operand
+            if (token == '(') {
+                if (expression) {
+                    token = expression.shift();
+                    rparser.verify_expression(expression, token, syntax_error);
+                } else {
+                    syntax_error = true;
+                }
+                if (!syntax_error) {
+                    if (token != ')') {
+                        syntax_error = true;
+                    } else if (expression) {
+                        token = expression.shift();
+                    }
+                }
+            } else {
                 syntax_error = true;
-            } else if (expression) {
+            }    
+        }
+    }
+}
+
+rparser.verify_mult_factor = (expression, token, syntax_error) => {
+    // valid multi-factor regardless of token because of NULL production
+    if (rparser.valid_operator(token)) {
+        if (token == '*') {
+            if (expression) {
                 token = expression.shift();
+                verify_factor(expression, token, syntax_error); 
+            } else {
+                syntax_error = true;
+            }
+            if (!syntax_error) {
+                rparser.verify_mult_factor(expression, token, syntax_error);
             }
         }
     }
 }
+
+rparser.verify_factor = (expression, token, syntax_error) => {
+    rparser.verify_primary(expression, token, syntax_error);
+    if (!syntax_error) {
+        rparser.verify_mult_factor(expression, token, syntax_error);
+    }
+}
+
+x = ['a','*','b','/','(','d','+','e',')','-','9'];
+console.log(rparser.top_level_expression(x));
+
