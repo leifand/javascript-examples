d29f71e3021b4e93a075e6bd3d1c142e42c1acdb
Leif Anderson <leif.v.anderson@gmail.com>
2019-02-04 08:44:43 -0600

    expand not reading from queue correctly

--- graph/search/SearchNode.js
+++ graph/search/SearchNode.js
@@ -1,6 +1,7 @@
 /*
     SearchNode.js
     leif anderson feb 3 2019
+    A node object for exploring graph traversal eg. TSP!
 */
 
 class SearchNode {
@@ -70,25 +71,30 @@ class SearchNode {
 
 const graph = {};
 
-let node = new SearchNode('Dallas', null);
+let node = new SearchNode('Dallas', 'GOAL');
 
 graph.dallas = node;
+graph.dallas.cost = 0; // Dallas is the starting point ...
 
 node = new SearchNode('El Paso', null);
 
 graph.elpaso = node;
+graph.elpaso.cost = 800; // 800 miles from Dallas ...
 
 node = new SearchNode('Amarillo', null);
 
 graph.amarillo = node;
+graph.amarillo.cost = 550; 
 
 node = new SearchNode('Midland', null);
 
 graph.midland = node;
+graph.midland.cost = 450;
 
 node = new SearchNode('Johnson City', null);
 
 graph.johnsoncity = node;
+graph.johnsoncity.cost = 400;
 
 graph.dallas.add_link(graph.midland);
 graph.dallas.add_link(graph.amarillo);
@@ -97,4 +103,29 @@ graph.dallas.add_link(graph.johnsoncity);
 graph.elpaso.add_link(graph.midland);
 graph.elpaso.add_link(graph.johnsoncity);
 
+graph.amarillo.add_link(graph.midland);
+graph.amarillo.add_link(graph.dallas);
+
+graph.midland.add_link(graph.dallas);
+graph.midland.add_link(graph.amarillo);
+graph.midland.add_link(graph.elpaso);
+
+graph.johnsoncity.add_link(graph.dallas);
+graph.johnsoncity.add_link(graph.elpaso);
+
 console.log(graph);
+
+function breadth_first(initial_node, goal_state)  { // complete search of state space, returns shortest path
+    let queue = [];
+    queue.push(initial_node);
+    initial_node.tested = true;
+    while(queue.length > 0) {
+        let test_node = queue.shift();
+        console.log(test_node.trace());
+        if(test_node.state == goal_state) return test_node;
+        if(!test_node.expanded) test_node.expand(queue,0);
+    }
+    return null;
+}
+
+console.log(breadth_first(graph.dallas,'GOAL'));
